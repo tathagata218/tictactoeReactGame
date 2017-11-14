612bc87c4ed14c4edccea796763cf5c63e17dc94
tathagata218 <tathagata218@yahoo.com>
2017-11-14 14:16:14 -0600

    this is the stuff

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Box from "./components/Box"
 import friends from "./friends.json";
 
 
+const divStyle = {
+float: "left",
+background: "green"
+}
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
@@ -14,32 +18,22 @@ class App extends Component {
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-       <div>
+  <div>
        <h1> This is the tictactoe game</h1>
     <div className = "container">
        <div className = "row">
-       <div calssName = "col-md-3 col-md-offset-3">
+       <div style={divStyle} calssName = "col-md-2" >
             <Box/>
+      </div>
+      <div style={divStyle} calssName = "col-md-2" >
             <Box/>
+      </div>
+      <div style={divStyle} calssName = "col-md-2" >
             <Box/>
-          <div className="row">
-          <div classname = "col-md-3">
-            <Box/>
-            <Box/>
-            <Box/>
-          </div>
-          </div>
-        <div className="row">
-        <div classname = "col-md-3">
-              <Box/>
-              <Box/>
-              <Box/>
-          </div>
-          </div>
-        </div>
-        </div>
+      </div>
+      </div>
     </div>
-       
+  </div>
        );
   }
 }
