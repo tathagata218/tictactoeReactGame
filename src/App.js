8ce58a7700a2ab54a1ee0e344e41c506f1a672bd
tathagata218 <tathagata218@yahoo.com>
2017-11-21 16:05:07 -0600

    this is the stuff

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
   
 
   checkWinner = () => {
-    let winnerCombo = [[0,1,2],[3,4,5],[4,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    let winnerCombo = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     let boardArr = this.state.board;
     let current_turn = this.state.current_player;
 
