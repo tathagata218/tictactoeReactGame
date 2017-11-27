f747efb001f3025364c509ced2cf77ba3cb1ae0f
tathagata218 <tathagata218@yahoo.com>
2017-11-27 17:53:29 -0600

    this is the stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 class App extends Component {
   
   state = { 
-      button_clicked : true
+      button_clicked : true,
+      winningCombos : [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
           };
 
   divClickfuncTwo  (index)  {
@@ -21,8 +22,8 @@ class App extends Component {
  
  
   
-  // console.log(this.state);
-    
+  console.log(this.state);
+    this.render();
 }
 
   divClickfuncOne (index){
@@ -98,7 +99,7 @@ class App extends Component {
         return false
       }
     })
-    this.render()  
+
   }
 
   render() {
