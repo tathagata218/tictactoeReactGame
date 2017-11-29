74bb387be87c1b2aa077318e0ff8fe8123f1265a
tathagata218 <tathagata218@yahoo.com>
2017-11-29 13:23:15 -0600

    this is the stuff

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   state = { 
       button_clicked : true,
       winningCombos : [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+      
           };
 
   divClickfuncTwo  (index)  {
