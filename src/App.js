f95dd5ef9f7d89187d25811f49ea7cbf6ef6d768
tathagata218 <tathagata218@yahoo.com>
2017-12-04 09:35:25 -0600

    this is the stuff

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
    
     
     setTimeout(randomNum, 1500);
-    setTimeout(this.render,1600);
+    setTimeout(console.log(this.state),1600);
 
     console.log(this.state);
 
